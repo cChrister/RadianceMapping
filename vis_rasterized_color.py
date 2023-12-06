5d135a893b4947be1b5179f85bd94f7ba63c15e4
renhaofan <yqykrhf@163.com>
2023-12-06 09:08:04 +0800

    script: vis

---
+++ vis_rasterized_color.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+my_dpi = 96
+
+# img = np.load("cat.npy") # [800, 800, 3]
+
+
+# threshold = 1e-4  # 设置阈值，根据实际情况调整
+# img[img < threshold] = 1.0
+
+
+# h, w, channels = img.shape
+
+# plt.figure(figsize=(h/my_dpi, w/my_dpi), dpi=my_dpi)
+# plt.imshow(img)
+# plt.axis('off')
+# plt.margins(0,0)
+# plt.savefig("cat.png", bbox_inches="tight", pad_inches=0.0)
+# plt.show()
--- vis_zbuffer.py
+++ vis_zbuffer.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 my_dpi = 96
-c_id = 100
-path = '0.005-z-800-test.npy'
+c_id = 40
+# path = '0.005-z-800-test.npy'
+
+path = "data/fragments/lego_pointnerf/0.005-z-800-1-test.npy"
 
 
 zbuf = np.load(path)
